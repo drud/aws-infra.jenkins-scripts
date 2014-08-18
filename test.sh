433cb098c3ea067da844805c3481a2ee42bd8009
Kevin Bridges <kevin@cyberswat.com>
2014-08-18 08:52:43 -0600

    Testing changes.

--- test.sh
+++ test.sh
@@ -5,8 +5,7 @@ export CHEF_VALIDATION_CLIENT_NAME=newmediadenver-validator
 export CHEF_CLIENT_KEY=/var/jenkins_home/.chef/chef_clientkey.pem
 export CHEF_NODE_NAME=jenkins_ac
 export CHEF_SERVER_URL=https://api.opscode.com/organizations/newmediadenver
-export COOKBOOK_NAME=`awk '$1=="name"{print $2}' $WORKSPACE/metadata.rb | sed "s/'//g"`
 
 env
 
-knife ssh -A "$COOKBOOK_NAME:action" "sudo chef-client" --ssh-user jenkins_ac
+knife ssh -A name:web001.newmediadenver.com sudo chef-client --ssh-user jenkins_ac
