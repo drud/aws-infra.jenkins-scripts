a7b9266dd53d17d3a7f4ce03d771398a6c70b42e
Kevin Bridges <kevin@cyberswat.com>
2014-09-07 13:49:34 -0600

    site archive tweaks.

--- site-archive.sh
+++ site-archive.sh
@@ -6,21 +6,8 @@ export CHEF_CLIENT_KEY=/var/jenkins_home/.chef/chef_clientkey.pem
 export CHEF_NODE_NAME=jenkins_ac
 export CHEF_SERVER_URL=https://api.opscode.com/organizations/newmediadenver
 if [ -z "$COOKBOOK_NAME" ]; then
-  export COOKBOOK_NAME="nmd$JOB_NAME"
+  export COOKBOOK_NAME="nmd$SITE"
 fi
 env
-
-case "$GIT_BRANCH" in
-  'origin/master')
-    CHEF_ENVIRONMENT='production'
-    ;;
-  'origin/staging')
-    CHEF_ENVIRONMENT='staging'
-    ;;
-  *)
-    CHEF_ENVIRONMENT='_default'
-    ;;
-esac
-
-knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}"
-knife ssh -A "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" "sudo -E -P chef-client" --ssh-user jenkins_ac
+CHEF_ENVIRONMENT='staging'
+knife ssh -A "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" "sudo -E -P /home/jenkins_ac/scripts/current/site-archive-child.sh" --ssh-user jenkins_ac
