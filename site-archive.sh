e277137e7890db3757022abed6ffcbc6710d657c
Kevin Bridges <kevin@cyberswat.com>
2014-09-07 17:22:39 -0600

    Explicitly setting dumb terminal.

--- site-archive.sh
+++ site-archive.sh
@@ -3,7 +3,7 @@
 env
 
 rm -f $WORKSPACE/*.tar.gz
-ssh -i /var/jenkins_home/.ssh/id_rsa jenkins_ac@$HOSTNAME "sudo -E -P /home/jenkins_ac/scripts/current/site-archive-child.sh $SITE"
+ssh -i /var/jenkins_home/.ssh/id_rsa jenkins_ac@$HOSTNAME "TERM=dumb sudo -E -P /home/jenkins_ac/scripts/current/site-archive-child.sh $SITE"
 current_size=$(ssh -i /var/jenkins_home/.ssh/id_rsa jenkins_ac@$HOSTNAME "du /tmp/$SITE.tar.gz" | awk {'print$1'})
 current_available=`df | grep /dev/mapper | awk -F ' ' '{print $4}'`
 current_buffer=500000
