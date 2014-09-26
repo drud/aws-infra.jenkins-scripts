67c6132300c02e5794869e9ccbe3c6f5dd33fcaf
Kevin Bridges <kevin@cyberswat.com>
2014-09-26 10:23:58 -0600

    Updating kinfe-download.

--- knife-download.sh
+++ knife-download.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -xe
+
 export NMDCHEF_NODE_NAME=jenkins_ac
 export NMDCHEF_CLIENT_KEY=/var/jenkins_home/.chef/chef_clientkey.pem
 export NMDCHEF_VALIDATION_CLIENT_NAME=newmediadenver-validator
 export NMDCHEF_VALIDATION_KEY=/var/jenkins_home/.chef/chef_validationkey.pem
 export NMDCHEF_SERVER_URL=https://api.opscode.com/organizations/newmediadenver
 export HOME=$WORKSPACE
+
 env
+
 git checkout packer-clean
 git fetch --all
 git pull
 knife download /roles
 knife download /data_bags
 knife download /environments
+rm -rf vendor/cookbooks/*
 knife download /cookbooks
 git config user.email "ops@newmediadenver.com"
 git config user.name "Jenkins"
