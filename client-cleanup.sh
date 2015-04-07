88b6b273b72ea9cbce3aa9bdce1985e8ed247900
tannerjfco <tannerjfco@gmail.com>
2015-04-07 15:41:21 -0600

    add chef cache cleanup

--- client-cleanup.sh
+++ client-cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 PRIVATEIP=$(knife search node "name:$HOSTNAME" | sed -n '6p' | awk '{print $2}')
-ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'rm -rf /root/.drush && rm -rf /tmp/*'
+ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'rm -rf /root/.drush && rm -rf /tmp/* && rm -rf /var/chef/cache/*.tar.gz'
