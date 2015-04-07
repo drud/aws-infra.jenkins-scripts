eed24834d0479cb22be537c2e44ac34a93f1f15a
tannerjfco <tannerjfco@gmail.com>
2015-04-07 15:21:55 -0600

    Adds client cleanup script

---
+++ client-cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -xe
+PRIVATEIP=$(knife search node "name:$HOSTNAME" | sed -n '6p' | awk '{print $2}')
+ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'rm -rf /root/.drush && rm -rf /tmp/*'
