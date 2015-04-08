0947e426629f2442b1b482da39440b71e1e6d43b
tannerjfco <tannerjfco@gmail.com>
2015-04-08 15:48:19 -0600

    add site-exec

---
+++ site-exec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -xe
+if [ -z "$COOKBOOK_NAME" ]; then
+  export COOKBOOK_NAME="nmd$JOB_NAME"
+fi
+
+env
+
+knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}" --verbose --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb
+PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb | sed -n '6p' | awk '{print $2}')
+ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug'
