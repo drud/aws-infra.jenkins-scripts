139a61f78d19d655c5fe75c5d31b5496f02a09b2
Kevin Bridges <kevin@cyberswat.com>
2015-04-09 09:58:04 -0600

    Adding a port to chef-client

--- chef-client.sh
+++ chef-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
 PRIVATEIP=$(knife search node "name:$HOSTNAME" | sed -n '6p' | awk '{print $2}')
-ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug'
+ssh -p$PORT -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug'
