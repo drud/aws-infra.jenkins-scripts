b60619732d7ca6fa0534d439c4e0252668e61918
Kevin Bridges <kevin@cyberswat.com>
2015-06-15 13:52:09 -0600

    What happens if I do this?

--- playbook.sh
+++ playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-ansible-playbook -i /etc/ansible/hosts -vvvv $WORKSPACE/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u root
+ansible-playbook -i /etc/ansible/hosts -vvvv $WORKSPACE/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u root --skip-tags "packages"
