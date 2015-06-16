e221f0ceb34e7bee2d5832c9951b5c61e0d08191
Kevin Bridges <kevin@cyberswat.com>
2015-06-16 14:28:25 -0600

    UPdaitng playbook.sh

--- playbook.sh
+++ playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-ansible-playbook -i /etc/ansible/hosts -vvvv $WORKSPACE/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u root --skip-tags "packages"
+ansible-playbook -i /etc/ansible/hosts -vvvv $JENKINS_HOME/workspace/jenkins-ansible/hosts/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u root --tags $2
