f0af3fc7c6b9c66fb2e9ea91a6c89fd53bceeac5
Kevin Bridges <kevin@cyberswat.com>
2015-06-18 09:18:30 -0600

    Passing in jumpcloud id to playbook runs

--- playbook.sh
+++ playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-ansible-playbook -i /etc/ansible/hosts -vvvv $JENKINS_HOME/workspace/jenkins-ansible/hosts/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u $3 --tags $2
+ansible-playbook -e "drud_jumpcloud_client_id=$DRUD_JUMPCLOUD_CLIENT_ID" -i /etc/ansible/hosts -vvvv $JENKINS_HOME/workspace/jenkins-ansible/hosts/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u $3 --tags $2
