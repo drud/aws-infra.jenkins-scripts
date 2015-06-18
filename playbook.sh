2d5df6254bc8de605e9199c7e18652a5c4a40c2e
Kevin Bridges <kevin@cyberswat.com>
2015-06-17 20:21:26 -0600

    passing in the user as an argument

--- playbook.sh
+++ playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -xe
-ansible-playbook -i /etc/ansible/hosts -vvvv $JENKINS_HOME/workspace/jenkins-ansible/hosts/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u root --tags $2
+ansible-playbook -i /etc/ansible/hosts -vvvv $JENKINS_HOME/workspace/jenkins-ansible/hosts/$1.yml --private-key /var/jenkins_home/.ssh/aws.pem -u $3 --tags $2
