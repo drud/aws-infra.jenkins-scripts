f1ebae2f46971b979936b9ee3a9075743bf5578f
Kevin Bridges <kevin@cyberswat.com>
2015-06-19 10:34:12 -0600

    Trying weird things.

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -13,36 +13,4 @@ fi;
 # Move into the jenkins directory
 cd $JENKINS_HOME
 git fetch --all
-
-#Add all top level xml files.
-git add *.xml
-
-# Add all job config.xml files.
-git add jobs/*/config.xml
-git add -f jobs/*/nextBuildNumber
-
-# Add all user config.xml files.
-git add users/*/config.xml
-
-# Add all user content files.
-git add userContent/*
-
-# Add plugins.
-git add plugins/*.hpi
-
-# Remove files from the remote repo that have been removed locally.
-git log --pretty=format: --name-only --diff-filter=D
-COUNT=`git log --pretty=format: --name-only --diff-filter=D | wc -l`
-if [ $COUNT -ne 0 ]
-  then git log --pretty=format: --name-only --diff-filter=D | xargs git rm
-fi
-
-# Commit the differences
-git commit -a -m "Automated commit of jenkins chaos"
-
-git fetch --all
-
-git rebase origin/master
-
-# Push the commit up to the remote repository.
-git push origin master
+git pull
