be3176f75cbf9fa41791a31a447261bdf23d9f0f
Kevin Bridges <kevin@cyberswat.com>
2015-06-19 10:03:00 -0600

    git fail

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -40,7 +40,7 @@ git commit -a -m "Automated commit of jenkins chaos"
 
 git fetch --all
 
-git rebase
+git rebase origin master
 
 # Push the commit up to the remote repository.
 git push origin master
