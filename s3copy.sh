eb0c98142be25750d089c53da89b67f9e7001905
tannerjfco <tannerjfco@gmail.com>
2015-06-24 11:58:23 -0600

    more fixes

--- s3copy.sh
+++ s3copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 env
 S3ARGS="-k ${AWS_ACCESS_KEY} -sk ${AWS_SECRET_KEY}"
-PRIVATEIP=$(knife exec -E "nodes.find('chef_environment:staging AND roles:web2') { |n| puts n.name }" --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb | sed -n '1p' | awk '{print $1}')
+PRIVATEIP=$(knife exec -E "nodes.find('chef_environment:staging AND roles:web2') { |n| puts n.name }" -c ${JENKINS_HOME}/workspace/jenkins-chef-client/.chef/knife.rb | sed -n '1p' | awk '{print $1}')
 LATEST=$(ssh -p22 -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP s3latest $S3ARGS nmdarchive $sitename/$from)
 S3FROM="s3://nmdarchive/${LATEST}"
 S3TO=`echo $S3FROM | sed s/$from/$to/`
--- site-exec.sh
+++ site-exec.sh
@@ -5,7 +5,7 @@ fi
 
 env
 
-knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}" --verbose --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb
-PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb | sed -n '6p' | awk '{print $2}')
+knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}" --verbose -c ${JENKINS_HOME}/workspace/jenkins-chef-client/.chef/knife.rb
+PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-chef-client/.chef/knife.rb | sed -n '6p' | awk '{print $2}')
 ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug'
-knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = 'sleep'; n.save}" --verbose --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb
+knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = 'sleep'; n.save}" --verbose-c ${JENKINS_HOME}/workspace/jenkins-chef-client/.chef/knife.rb
