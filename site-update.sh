eb3c1c12b4f7e2bf4e8cd12d61075b9e2117103a
Kevin Bridges <kevin@cyberswat.com>
2015-06-24 11:14:18 -0600

    No comment .. not sure what to say.

--- site-update.sh
+++ site-update.sh
@@ -17,7 +17,7 @@ case "$GIT_BRANCH" in
     ;;
 esac
 
-knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}" --verbose --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb
+knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}" --verbose -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb
 PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
 ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug'
-knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = 'sleep'; n.save}" --verbose --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb
+knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = 'sleep'; n.save}" --verbose -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb
