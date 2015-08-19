c0a551bf3210f9407c755e6625d75a2b52297677
Kevin Bridges <kevin@cyberswat.com>
2015-08-19 15:34:49 -0600

    Updating knife path.

--- client-cleanup.sh
+++ client-cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
-PRIVATEIP=$(knife search node "name:$HOSTNAME" --config /var/jenkins_home/workspace/jenkins-chef-client/.chef/knife.rb | sed -n '6p' | awk '{print $2}')
+PRIVATEIP=$(knife search node "name:$HOSTNAME" --config ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '6p' | awk '{print $2}')
 ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'rm -rf /root/.drush && rm -rf /tmp/* && rm -rf /var/chef/cache/*.tar.gz'
