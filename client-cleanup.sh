05b386b979495e9507e602103fb355020a5fd162
Kevin Bridges <kevin@cyberswat.com>
2015-08-19 15:39:21 -0600

    Chaging awk position

--- client-cleanup.sh
+++ client-cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -xe
-PRIVATEIP=$(knife search node "name:$HOSTNAME" --config ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '6p' | awk '{print $2}')
+PRIVATEIP=$(knife search node "name:$HOSTNAME" --config ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
 ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'rm -rf /root/.drush && rm -rf /tmp/* && rm -rf /var/chef/cache/*.tar.gz'
