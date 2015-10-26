49fd9bda0ed4c80b041375e455dc02b784b5709a
Nate Hero <nate@newmediadenver.com>
2015-10-26 13:59:34 -0600

    moving commands

--- drupal-available-updates.sh
+++ drupal-available-updates.sh
@@ -3,7 +3,4 @@
 env
 
 PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
-ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug' 
-cd /var/www/infbuyer/current/docroot
-ls -lah
-drush ups
+ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'cd /var/www/infbuyer/current/docroot && ls -lah && drush ups' 
