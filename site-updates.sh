e1789aae6a13bb97770acaa774fedf10002a3ee3
Nate Hero <nate@newmediadenver.com>
2015-10-26 13:51:44 -0600

    more testing

--- site-updates.sh
+++ site-updates.sh
@@ -4,6 +4,6 @@ env
 
 PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
 ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug' 
-/var/www/infbuyer/current/docroot
+cd /var/www/infbuyer/current/docroot
 ls -lah
 drush ups
