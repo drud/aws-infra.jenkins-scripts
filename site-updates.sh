a2f377d1286719e3429e98292bad5df2438edb9e
Nate Hero <nate@newmediadenver.com>
2015-10-26 11:31:41 -0600

    testing changes

--- site-updates.sh
+++ site-updates.sh
@@ -5,6 +5,5 @@ fi
 
 env
 
-knife exec -E "nodes.find('chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action') { |n| n.normal.$COOKBOOK_NAME.action = '$CHEF_ACTION'; n.save}" --verbose -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb
-PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
+PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:update" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
 ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'chef-client -l debug' 
