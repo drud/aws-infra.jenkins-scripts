50ab2fcd024fc85d61cefe7204325d98014e494b
Nate Hero <nate@newmediadenver.com>
2015-10-30 11:12:49 -0600

    moving to multiple lines for readability

--- drupal-available-updates.sh
+++ drupal-available-updates.sh
@@ -3,6 +3,17 @@
 env
 
 PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $COOKBOOK_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
-ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP 'cd /var/www/ && for d in */ ; 
-  do version_docroot=$(drush -p5.5 -r /var/www/$d/current/docroot st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); version_current=$(drush -p5.5 -r /var/www/$d/current st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); if [[ -n "$version_docroot" || -n "$version_current" ]]; then echo "Drupal $version_docroot $version_current Site: $d"; if [[ -n "$version_docroot" ]]; then drush -p5.5 -r /var/www/$d/current/docroot ups; else  drush -p5.5 -r /var/www/$d/current ups; fi; fi; 
-  done'
+ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP '
+  cd /var/www/ && for d in */ ; 
+    do 
+      version_docroot=$(drush -p5.5 -r /var/www/$d/current/docroot st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
+      version_current=$(drush -p5.5 -r /var/www/$d/current st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
+
+      if [[ -n "$version_docroot" || -n "$version_current" ]]; 
+        then echo "Drupal $version_docroot $version_current Site: $d"; 
+          if [[ -n "$version_docroot" ]]; 
+            then drush -p5.5 -r /var/www/$d/current/docroot ups; 
+            else  drush -p5.5 -r /var/www/$d/current ups; 
+          fi; 
+      fi; 
+    done'
