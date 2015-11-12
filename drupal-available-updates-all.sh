7bfe0db65c0eb23ec9a591863a3a819126c271b7
Nate Hero <nate@newmediadenver.com>
2015-11-12 13:40:16 -0700

    testing

--- drupal-available-updates-all.sh
+++ drupal-available-updates-all.sh
@@ -16,9 +16,9 @@ do
   echo -e "\n Checking server: $i\n"
   PRIVATEIP=$(knife search node "name:$i" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
   ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP '
-    declare -A sites
-    declare -A sites_version
-    declare -A sites_updates
+    #declare -A sites
+    #declare -A sites_version
+    #declare -A sites_updates
 
     cd /var/www/ && for d in */ ; 
     do 
@@ -32,23 +32,25 @@ do
               UPS="$(drush -p5.5 -r /var/www/$d/current/docroot ups 2>/dev/null)" &&
               echo "${UPS}" > /var/tmp/tmp.txt && 
               errors="$(wc -l /var/tmp/tmp.txt | grep -o [0-9][0-9])" &&
-              sites[$site]="${UPS}"
-              sites_version[$site]=$version_docroot
-              sites_updates[$site]=$errors
+              echo $errors
+              #sites[$site]="${UPS}"
+              #sites_version[$site]=$version_docroot
+              #sites_updates[$site]=$errors
             else 
               UPS="$(drush -p5.5 -r /var/www/$d/current ups 2>/dev/null)" &&
               echo "${UPS}" > /var/tmp/tmp.txt && 
               errors="$(wc -l /var/tmp/tmp.txt | grep -o [0-9][0-9])" &&
-              sites[$site]="${UPS}"
-              sites_version[$site]=$version_current
-              sites_updates[$site]=$errors
+              echo $errors
+              #sites[$site]="${UPS}"
+              #sites_version[$site]=$version_current
+              #sites_updates[$site]=$errors
           fi; 
       fi; 
     done
     
-    for key in ${!sites[@]}; do
-      printf  "Site: %-20s Drupal version: %-20s Available updates: %-20s\n" ${key} ${sites_version[${key}]} ${sites_updates[${key}]}
+    #for key in ${!sites[@]}; do
+      #printf  "Site: %-20s Drupal version: %-20s Available updates: %-20s\n" ${key} ${sites_version[${key}]} ${sites_updates[${key}]}
       #echo "${sites[${key}]}"
-    done
+    #done
     '
 done
