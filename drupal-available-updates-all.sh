673259d44647a405c36cf609005e6b9082be6fa0
Nate Hero <nate@newmediadenver.com>
2015-11-12 10:37:54 -0700

    testing

--- drupal-available-updates-all.sh
+++ drupal-available-updates-all.sh
@@ -17,7 +17,6 @@ do
   PRIVATEIP=$(knife search node "name:$i" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
   ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP '
     declare -A sites
-    sites["Sites on $i"]=Updates
     cd /var/www/ && for d in */ ; 
     do 
       site=$(echo $d | sed 's:/*$::')
@@ -27,15 +26,17 @@ do
         then echo "Drupal $version_docroot$version_current Site: $site"; 
           if [[ -n "$version_docroot" ]]; 
             then 
-              UPS="$(drush -p5.5 -r /var/www/$d/current/docroot ups)" &&
-              echo "${UPS}" > /var/tmp/tmp.txt && 
-              errors="$(wc -l /var/tmp/tmp.txt | grep -o [0-9][0-9])" &&
-              sites[$site]=$errors
+              drush -p5.5 -r /var/www/$d/current/docroot ups
+              #UPS="$(drush -p5.5 -r /var/www/$d/current/docroot ups)" &&
+              #echo "${UPS}" > /var/tmp/tmp.txt && 
+              #errors="$(wc -l /var/tmp/tmp.txt | grep -o [0-9][0-9])" &&
+              #sites[$site]=$errors
             else 
-              UPS="$(drush -p5.5 -r /var/www/$d/current ups)" &&
-              echo "${UPS}" > /var/tmp/tmp.txt && 
-              errors="$(wc -l /var/tmp/tmp.txt | grep -o [0-9][0-9])" &&
-              sites[$site]=$errors
+              drush -p5.5 -r /var/www/$d/current ups
+              #UPS="$(drush -p5.5 -r /var/www/$d/current ups)" &&
+              #echo "${UPS}" > /var/tmp/tmp.txt && 
+              #errors="$(wc -l /var/tmp/tmp.txt | grep -o [0-9][0-9])" &&
+              #sites[$site]=$errors
           fi; 
       fi; 
     done
