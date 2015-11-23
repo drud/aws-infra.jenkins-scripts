2313f4b3a8f206c3a55ee3c5e603518485fd0b54
Nate Hero <nate@newmediadenver.com>
2015-11-23 14:23:23 -0700

    adding single site lookup

--- drupal-available-updates-all.sh
+++ drupal-available-updates-all.sh
@@ -9,29 +9,58 @@ else
   arr=($HOSTNAME)
 fi
 
-for i in "${arr[@]}"
-do
-  echo -e "\n Checking server: $i\n"
-  PRIVATEIP=$(knife search node "name:$i" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
-  ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP "DETAILS=$DETAILS SERVER=$i" '
-    cd /var/www/ && for d in */ ; 
-    do 
-      site=$(echo $d | sed 's:/*$::')
-      version_docroot=$(drush -p5.5 -r /var/www/$d/current/docroot st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
-      version_current=$(drush -p5.5 -r /var/www/$d/current st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
-      if [[ -n "$version_docroot" || -n "$version_current" ]]; 
-        then
-          if [[ -n "$version_docroot" ]]; 
-            then 
-              UPS="$(drush -p5.5 -r /var/www/$d/current/docroot ups 2>/dev/null)" &&
-              version=$version_docroot
-            else 
-              UPS="$(drush -p5.5 -r /var/www/$d/current ups 2>/dev/null)" &&
-              version=$version_current 
-          fi;
-          updates=$(grep -ci "update" <<< "$UPS") &&
-          echo "Checking Drupal $version site: $site on server: $SERVER has $updates updates available" &&
-          if [[ $DETAILS = true ]]; then echo -e "${UPS}" "\n"; fi;
-      fi; 
-    done'
-done
+CHEF_ENVIRONMENT='staging'
+if [[ -n "$SITE_NAME" ]];
+  then
+    PRIVATEIP=$(knife search node "chef_environment:$CHEF_ENVIRONMENT AND $SITE_NAME:action" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
+    ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP "DETAILS=$DETAILS" '
+      cd /var/www/$SITE_NAME
+      do 
+        site=$SITE_NAME
+        d=$SITE_NAME
+        version_docroot=$(drush -p5.5 -r /var/www/$d/current/docroot st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
+        version_current=$(drush -p5.5 -r /var/www/$d/current st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
+        if [[ -n "$version_docroot" || -n "$version_current" ]]; 
+          then
+            if [[ -n "$version_docroot" ]]; 
+              then 
+                UPS="$(drush -p5.5 -r /var/www/$d/current/docroot ups 2>/dev/null)" &&
+                version=$version_docroot
+              else 
+                UPS="$(drush -p5.5 -r /var/www/$d/current ups 2>/dev/null)" &&
+                version=$version_current 
+            fi;
+            updates=$(grep -ci "update" <<< "$UPS") &&
+            echo "Checking Drupal $version site: $site has $updates updates available" &&
+            if [[ $DETAILS = true ]]; then echo -e "${UPS}" "\n"; fi;
+        fi; 
+      done
+    '
+else
+  for i in "${arr[@]}"
+  do
+    echo -e "\n Checking server: $i\n"
+    PRIVATEIP=$(knife search node "name:$i" -c ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
+    ssh -A -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP "DETAILS=$DETAILS SERVER=$i" '
+      cd /var/www/ && for d in */ ; 
+      do 
+        site=$(echo $d | sed 's:/*$::')
+        version_docroot=$(drush -p5.5 -r /var/www/$d/current/docroot st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
+        version_current=$(drush -p5.5 -r /var/www/$d/current st | grep "Drupal version" | grep -o [678][.] | grep -o [678]); 
+        if [[ -n "$version_docroot" || -n "$version_current" ]]; 
+          then
+            if [[ -n "$version_docroot" ]]; 
+              then 
+                UPS="$(drush -p5.5 -r /var/www/$d/current/docroot ups 2>/dev/null)" &&
+                version=$version_docroot
+              else 
+                UPS="$(drush -p5.5 -r /var/www/$d/current ups 2>/dev/null)" &&
+                version=$version_current 
+            fi;
+            updates=$(grep -ci "update" <<< "$UPS") &&
+            echo "Checking Drupal $version site: $site on server: $SERVER has $updates updates available" &&
+            if [[ $DETAILS = true ]]; then echo -e "${UPS}" "\n"; fi;
+        fi; 
+      done'
+  done
+fi
