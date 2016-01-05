83889fa8415d628b4efeba8963da7e0ccd61f6e7
owenbush <owen@newmediadenver.com>
2016-01-05 16:01:41 -0700

    Adding cleanup-releases scripts

---
+++ cleanup-releases-exec.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+$COUNT=$1
+
+cd  /var/www/;
+for dir in `ls` ;
+do
+  echo '----------------------------';
+  echo "Analyzing $dir";
+  a=0;
+	b=0;
+	# Get the current symlink so we can ignore it
+  current=`readlink /var/www/$dir/current`;
+  current=${current:9}
+  echo "Current release: $current"
+  for i in `ls -t /var/www/$dir/releases --ignore='.[^.]' --ignore='$current'`;
+  do
+    a=`expr $a + 1`;
+      if [ $a -gt $COUNT ]; then
+        b=`expr $b + 1`;
+        echo "Deleting: '/var/www/$dir/releases/$i";
+        # rm -rf /var/www/$dir/releases/$i
+      fi;
+  done
+	echo "Found $a release(s) that are not the current release. Deleted $b";
+	echo '----------------------------';
+done
