15709fa7d904c279d3d7b850d613e9ddd7bab02c
owenbush <owen@newmediadenver.com>
2016-01-06 12:26:54 -0700

    Extra check to not remove current, and disk space added

--- cleanup-releases-exec.sh
+++ cleanup-releases-exec.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Free Disk Space Before:"
+echo "$(df -h)"
+echo '----------------------------';
+
 cd  /var/www/;
 for dir in `ls` ;
 do
@@ -15,11 +19,17 @@ do
   do
     a=`expr $a + 1`;
       if [[ $a -gt $1 ]]; then
-        b=`expr $b + 1`;
-        echo "Deleting: '/var/www/$dir/releases/$i";
-        # rm -rf /var/www/$dir/releases/$i
+        if [[ "$i" -ne "$current" ]]; then
+          b=`expr $b + 1`;
+          echo "Deleting: '/var/www/$dir/releases/$i";
+          rm -rf /var/www/$dir/releases/$i
+        fi;
       fi;
   done
         echo "Found $a release(s) that are not the current release. Deleted $b";
         echo '----------------------------';
 done
+
+echo "Free Disk Space After:"
+echo "$(df -h)"
+echo '----------------------------';
