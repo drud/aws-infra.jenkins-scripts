0d3f87a597c167ad8d82d525ae90b8d61f956623
owenbush <owen@newmediadenver.com>
2016-01-06 11:27:34 -0700

    Lets try execute the local bash script on the remote server

--- cleanup-releases-init.sh
+++ cleanup-releases-init.sh
@@ -2,4 +2,4 @@
 env
 COUNT="$COUNT"
 PRIVATEIP=$(knife search node "name:$HOSTNAME" --config ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
-ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP cleanup-releases-exec.sh $COUNT
+ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP "bash -s --" < ${JENKINS_HOME}/workspace/jenkins-scripts/cleanup-releases-exec.sh $COUNT
