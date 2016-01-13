c0efe3a0f16378f5f266f9ed727b4a912f6251ac
owenbush <owen@newmediadenver.com>
2016-01-13 11:15:12 -0700

    Updated file mode and removed knife stuff to get private ip

--- cleanup-releases-init.sh
+++ cleanup-releases-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 env
 COUNT="$COUNT"
-PRIVATEIP=$(knife search node "name:$HOSTNAME" --config ${JENKINS_HOME}/workspace/jenkins-scripts/.chef/knife.rb | sed -n '4p' | awk '{print $2}')
-ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$PRIVATEIP "bash -s --" < ${JENKINS_HOME}/workspace/jenkins-scripts/cleanup-releases-exec.sh $COUNT
+ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no root@$HOSTNAME "bash -s --" < ${JENKINS_HOME}/workspace/jenkins-scripts/cleanup-releases-exec.sh $COUNT
