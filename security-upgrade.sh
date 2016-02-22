98f66168cafc9190d91fb7a609643c6c616a8785
csterling <csterling@newmediadenver.com>
2016-02-22 14:03:52 -0700

    This should install the package if it doesn't exist, agnostic of the OS.

--- security-upgrade.sh
+++ security-upgrade.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
-#HOST="monitor01.nmdev.us"
+#HOSTNAME="monitor01.nmdev.us"
 #USER=""
 
+#Retain whitespace in variables
+IFS='%'
+# Usage yum_package_exists PKG_NAME
+function yum_package_exists() {
+  if yum list installed "$1" >/dev/null 2>&1; then
+    true
+  else
+    false
+  fi
+}
+# Usage apt_package_exists PKG_NAME
+function apt_package_exists() {
+	return dpkg -l "$1" &> /dev/null
+}
+# We override this string only if the package isn't installed.
+INSTALL_CMD=""
 # Try an SSH command to ubuntu first (faster that if root first)
 RETURN=$(ssh -q -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no -o NumberOfPasswordPrompts=0 ubuntu@$HOSTNAME 'exit')
 if [ $? -eq 0 ]; then
+  echo "This is an ubuntu machine."
   USER="ubuntu"
   UP_CMD="sudo -i
   unattended-upgrade -v"
+  INSTALL_CMD="sudo -i
+    typeset -f | if ! apt_package_exists unattended-upgrade ; then
+	  apt-get -y update &&
+	  echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections &&
+	  apt-get -y install unattended-upgrades &&
+	  dpkg-reconfigure -f noninteractive unattended-upgrades;
+	fi"
 else
+	echo "We *think* this is a CentOS box."
   USER="root"
   UP_CMD="yum update -y --security;"
+  INSTALL_CMD="typeset -f | if ! yum_package_exists yum-plugin-security ; then
+    yum install -y yum-plugin-security
+	fi"
 fi
-ssh -q -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no $USER@$HOSTNAME <<EOF
+ssh -T -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no $USER@$HOSTNAME<<EOF
+  $(echo $INSTALL_CMD)
   $(echo $UP_CMD)
+  echo "The previous commands ran on $( uname -a )"
+  exit
 EOF
+unset IFS
