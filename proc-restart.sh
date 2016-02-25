0e7e8b0d7b1f9e90414d537c0548ce5fdfae0438
csterling <csterling@newmediadenver.com>
2016-02-25 14:00:16 -0700

    Adding the override logic.

--- proc-restart.sh
+++ proc-restart.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 # Must pass $PROCNAME, HOSTNAME, OS into the env.
-# PROCNAME=""
 if [ -z "$PROCNAME" ]; then
-        echo "PROCNAME is undefined"
-        exit 1;
+	echo "PROCNAME is undefined"
+	exit 1;
 fi
 IFS="%"
 function proc_count (){
-        local PROC=$1
-        local PROC_LIST_CMD='ps aux | grep "$PROC"'
-        local PROC_LIST=`eval $PROC_LIST_CMD`
-        echo "Processes matching '$PROC':"
-        echo $PROC_LIST
-        NUM_PROCS=`pgrep $PROC | wc -l`
-        echo "Number of '$PROC' processes found (excluding grep lines): $NUM_PROCS"
+	local PROC=$1
+	local PROC_LIST_CMD='ps aux | grep "$PROC"'
+	local PROC_LIST=`eval $PROC_LIST_CMD`
+	echo "Processes matching '$PROC':"
+	echo $PROC_LIST
+	NUM_PROCS=`pgrep $PROC | wc -l`
+	echo "Number of '$PROC' processes found (excluding grep lines): $NUM_PROCS"
 }
+echo "--------------"
 proc_count "$PROCNAME"
+# By default, we want to perform a restart
+OP="restart"
+# BUT if $FORCEOP is set
+if [ "$FORCEOP" != "no" ]; then
+	echo "Setting operation to '$FORCEOP'"
+	OP=$FORCEOP
+fi
 if [ "$OS" = "CENTOS" ]; then
-        RESTART_CMD="/etc/init.d/$PROCNAME restart"
+	CMD="/etc/init.d/$PROCNAME $OP"
 else
-        RESTART_CMD="service $PROCNAME restart"
+	CMD="service $PROCNAME $OP"
 fi
 
-if [ $NUM_PROCS -lt 1 ]; then
-        echo "No matching process found! Restarting '$PROCNAME'..."
-        eval $RESTART_CMD
+if [ $NUM_PROCS -lt 1 -a "$FORCEOP" = "no" ]; then
+	echo "No matching process found! Restarting '$PROCNAME'..."
+	eval $CMD
+elif [ "$FORCEOP" != "no" ]; then
+	echo "Performing '$CMD'"
+	eval $CMD
 else
-        echo "Found $NUM_PROCS running processes - We're good...right?"
+	echo "Found $NUM_PROCS running processes - We're good...right?"
 fi
+echo "--------------"
 unset IFS
