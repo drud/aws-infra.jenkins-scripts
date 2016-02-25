62129db2133b61c3209d8499576f75cf28f56bd1
csterling <csterling@newmediadenver.com>
2016-02-25 13:38:57 -0700

    Cleaning up some bad logic.

--- proc-restart.sh
+++ proc-restart.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 # Must pass $PROCNAME, HOSTNAME, OS into the env.
 # PROCNAME=""
+if [ -z "$PROCNAME" ]; then
+        echo "PROCNAME is undefined"
+        exit 1;
+fi
 IFS="%"
-proc_count (){
-	local PROC=$1
-	local PROC_LIST_CMD='ps aux | grep "$PROC"'
-	local PROC_LIST=`eval $PROC_LIST_CMD`
-	echo "Processes matching '$PROC':"
-	echo $PROC_LIST
-	NUM_PROCS=`pgrep $PROC | wc -l`
-	echo "Number of '$PROC' processes found: $NUM_PROCS"
+function proc_count (){
+        local PROC=$1
+        local PROC_LIST_CMD='ps aux | grep "$PROC"'
+        local PROC_LIST=`eval $PROC_LIST_CMD`
+        echo "Processes matching '$PROC':"
+        echo $PROC_LIST
+        NUM_PROCS=`pgrep $PROC | wc -l`
+        echo "Number of '$PROC' processes found (excluding grep lines): $NUM_PROCS"
 }
-sudo -i
 proc_count "$PROCNAME"
 if [ "$OS" = "CENTOS" ]; then
-	RESTART_CMD="/etc/init.d/$PROCNAME restart"
+        RESTART_CMD="/etc/init.d/$PROCNAME restart"
 else
-	RESTART_CMD="service $PROCNAME restart"
+        RESTART_CMD="service $PROCNAME restart"
 fi
 
 if [ $NUM_PROCS -lt 1 ]; then
-	echo "No matching process found! Restarting '$PROCNAME'..."
-	eval $RESTART_CMD
+        echo "No matching process found! Restarting '$PROCNAME'..."
+        eval $RESTART_CMD
 else
-	echo "Found $NUM_PROCS running processes - We're good...right?"
+        echo "Found $NUM_PROCS running processes - We're good...right?"
 fi
 unset IFS
