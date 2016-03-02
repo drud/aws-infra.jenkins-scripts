bf346993b391de65bfbb10784fa49c3a412e3db6
csterling <csterling@newmediadenver.com>
2016-03-01 17:09:26 -0700

    Trying a little different syntax - more quoting.

--- security-upgrade-wrapper.sh
+++ security-upgrade-wrapper.sh
@@ -1,17 +1,17 @@
 #Retain whitespace in variables
-IFS='%'
 # SCRIPTNAME, HOSTNAME, ENVOVERLOAD
 ENVOVERLOAD=""
 SCRIPTNAME="security-upgrade-remote.sh"
 # We override this string only if the package isn't installed.
 INSTALL_CMD=""
-PROXY_CMD=$($JENKINS_HOME/workspace/jenkins-scripts/determine-proxy.sh $HOSTNAME)
-OS=$($JENKINS_HOME/workspace/jenkins-scripts/determine-os.sh $HOSTNAME)
-if [ $OS = "UBUNTU" ]; then
+PROXY_CMD="`$JENKINS_HOME/workspace/jenkins-scripts/determine-proxy.sh $HOSTNAME`"
+OS="`$JENKINS_HOME/workspace/jenkins-scripts/determine-os.sh $HOSTNAME`"
+
+if [ "$OS" = "UBUNTU" ]; then
 	$USER="ubuntu"
 else
 	$USER="root"
 fi
-SSH_CMD=$($JENKINS_HOME/workspace/jenkins-scripts/ssh-generator.sh $SCRIPTNAME $ENVOVERLOAD)
+SSH_CMD="`$JENKINS_HOME/workspace/jenkins-scripts/ssh-generator.sh $SCRIPTNAME $ENVOVERLOAD`"
 echo "Here is the SSH CMD: $SSH_CMD"
 eval "$SSH_CMD"
--- ssh-generator.sh
+++ ssh-generator.sh
@@ -1,15 +1,15 @@
 # !/bin/bash
 # HOSTNAME
 # $1=SCRIPTNAME
-if [ -z $1 -o -z $2 ]; then
+if [ -z "$1" -o -z "$2" ]; then
 	echo "This script requires that you pass in a scriptname to run."
 	echo "Usage: ./ssh-generator.sh SCRIPTNAME ENVOVERLOAD"
 	exit 1;
 fi
 SCRIPTNAME=$1
 ENVOVERLOAD=$2
-PROXY_CMD=$($JENKINS_HOME/workspace/jenkins-scripts/determine-proxy.sh $HOSTNAME)
-OS=$($JENKINS_HOME/workspace/jenkins-scripts/determine-os.sh $HOSTNAME)
+PROXY_CMD="`$JENKINS_HOME/workspace/jenkins-scripts/determine-proxy.sh $HOSTNAME`"
+OS="`$JENKINS_HOME/workspace/jenkins-scripts/determine-os.sh $HOSTNAME`"
 if [ "$OS" = "UBUNTU" ]; then
 	USER="ubuntu"
 else
