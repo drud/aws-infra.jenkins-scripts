410a41f704e86ea8c57b5496176a4b161418cdb2
tannerjfco <tannerjfco@gmail.com>
2016-05-02 14:23:56 -0600

    cd to get desired archive contents

--- log-capture.sh
+++ log-capture.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 FILENAME=$(hostname)-$(date +%s)-error-log.tar.gz
 # zip and upload
-tar -czf /tmp/$FILENAME . -C /var/log/nginx;
+cd /var/log/nginx && tar -czf /tmp/$FILENAME .;
 s3upload -l DEBUG -k $AWS_ACCESS_KEY -sk $AWS_SECRET_KEY -f -np 8 -s 100 /tmp/$FILENAME s3://nmdarchive/error-log/$FILENAME;
 # purge logs from server
 find /var/log/nginx -type f -exec sh -c '>{}' \;
