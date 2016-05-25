d2569b776b968d9d49ee4e4b4e7a587873614e61
csterling <csterling@newmediadenver.com>
2016-05-25 10:34:18 -0600

    Cleaning up.

--- find-web-roots-and-shas.sh
+++ find-web-roots-and-shas.sh
@@ -1,17 +1,4 @@
-# Find the SHA of the current site pointer
-find /var/www -type l -name current -print -exec git --git-dir={}/.git rev-parse HEAD \;
+#!/bin/bash
 
-# Trigger jenkins update for each job
-  # Parse apart the file string - get the databag name
-  # Parse apart the git remote -v - get the repo name
-
-  # Trigger {env}-{databag} jenkins job
-
-# Copy with rsync
-
-
-
-
-
-web02 catch-up - done: staging-gradeem,staging-thestonecollection,staging-dig-smart,staging-ca
-todo: staging-gretty
+ROOTS_AND_SHAS=$(find /var/www -maxdepth 2 -type l -name current -print -exec git --git-dir={}/.git rev-parse HEAD \;)
+echo $ROOTS_AND_SHAS
