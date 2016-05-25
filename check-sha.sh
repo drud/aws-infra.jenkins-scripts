e254a695a72c351a9e68ccfdd9929ab7d5604b19
csterling <csterling@newmediadenver.com>
2016-05-25 10:33:14 -0600

    Have to SET the variables...

--- check-sha.sh
+++ check-sha.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
+if [ -z "$1" -o -z "$2" ]; then
   echo "This script requires that you pass in the path to a webroot and the sha to compare to run."
   echo "Usage: ./check-sha WEBROOT GOODSHA"
   exit 1;
 fi
+WEBROOT=$1
+GOODSHA=$2
 
 SHA=$(git --git-dir=$WEBROOT/.git rev-parse HEAD)
 if [ "$SHA" != "$GOODSHA" ]; then
