69c105422ae7f5a58d7931a92688ebdaa4e484f8
csterling <csterling@newmediadenver.com>
2016-05-25 10:35:46 -0600

    Some general clean-up.

--- file-system-sync.sh
+++ file-system-sync.sh
@@ -7,7 +7,7 @@ OLD_SERVER="web02.nmdev.us"
 IFS=$'\n' read -rd '' -a y <<<"$x";
 #1. SSH into a good server and pull all the sites and SHAS from /var/www/*/current with find command
 HOSTNAME=$GOOD_SERVER
-WEBROOTS_AND_SHAS=$(./ssh-generator find-web-roots-and-shas.sh)
+WEBROOTS_AND_SHAS=$(./ssh-generator.sh find-web-roots-and-shas.sh)
 
 HOSTNAME=$OLD_SERVER
 for ROOT_SHA in $WEBROOTS_AND_SHAS; do
@@ -17,7 +17,7 @@ for ROOT_SHA in $WEBROOTS_AND_SHAS; do
 done
 
 #2. Given a site path, find the SHA on the desynced server and pass it back
-GOOD_SHA=$(./ssh-generator check-sha.sh $WEBROOT $SHA)
+GOOD_SHA=$(./ssh-generator.sh check-sha.sh $WEBROOT $SHA)
 
-3. Given a site path and a SHA, go to the given path and check the SHA. If the SHA doesn't match the first one, take the site path and parse out /var/www/<THIS DIR NAME> and set that equal to the databag name. Now, if the server ends in *.nmdev.us, ENV=staging. If server ends in *.newmediadenver.com, ENV=production. Else, fail
-Trigger jenkins job: {ENV}-{databag_name}
+#3. Given a site path and a SHA, go to the given path and check the SHA. If the SHA doesn't match the first one, take the site path and parse out /var/www/<THIS DIR NAME> and set that equal to the databag name. Now, if the server ends in *.nmdev.us, ENV=staging. If server ends in *.newmediadenver.com, ENV=production. Else, fail
+#Trigger jenkins job: {ENV}-{databag_name}
