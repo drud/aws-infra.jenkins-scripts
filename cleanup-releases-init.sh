05262de45e86d57da5efbfeaa34c85f13d2ccc19
csterling <csterling@newmediadenver.com>
2016-05-26 13:41:27 -0600

    Maybe if I don't wrap it so much, it will show more verbose output.

--- cleanup-releases-init.sh
+++ cleanup-releases-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -xe
 env
 COUNT="$COUNT"
-RESULT=(`eval $(/var/jenkins_home/workspace/jenkins-scripts/ssh-generator.sh "cleanup-releases-exec.sh $COUNT" env)`)
-echo $RESULT
+eval $(/var/jenkins_home/workspace/jenkins-scripts/ssh-generator.sh "cleanup-releases-exec.sh $COUNT" env)
