c931fab203ffdfdc6741d43e1b5c015ace32d185
csterling <csterling@newmediadenver.com>
2016-06-01 12:24:41 -0600

    Escaping the original command.

--- mysql/convert_to_innodb.sh
+++ mysql/convert_to_innodb.sh
@@ -10,9 +10,9 @@ DESYNC_NODE="SET GLOBAL wsrep_OSU_method='RSU';"
 
 # Find all the myisam tables
 FIND_ALL_TABLES="SELECT table_schema, TABLE_NAME, table_rows,
-CONCAT(ROUND((index_length+data_length)/1024/1024),\'MB\') AS size 
+CONCAT\(ROUND\(\(index_length+data_length\)/1024/1024\),\'MB\'\) AS size 
 FROM information_schema.TABLES   
-WHERE engine=\'myisam\' AND table_schema NOT IN (\'information_schema\', \'performance_schema\', \'mysql\');"
+WHERE engine=\'myisam\' AND table_schema NOT IN \(\'information_schema\', \'performance_schema\', \'mysql\'\);"
 
 # Change a myisam table to InnoDB
 CHANGE_TABLE="ALTER TABLE dbname.table_name engine = InnoDB;"
