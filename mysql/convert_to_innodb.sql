34d410d40277c6e049ecbcd8c823b6dd739e1b12
csterling <csterling@newmediadenver.com>
2016-06-10 15:57:17 -0600

    MySQL check-point.

--- mysql/convert_to_innodb.sql
+++ mysql/convert_to_innodb.sql
@@ -15,3 +15,8 @@ CHANGE_TABLE="ALTER TABLE dbname.table_name engine = InnoDB;"
 
 # Do this on each node to finish it off.
 SYNC_NODE="SET GLOBAL wsrep_OSU_method='TOI';"
+
+SELECT table_schema, TABLE_NAME, table_rows,
+CONCAT(ROUND((index_length+data_length)/1024/1024),'MB') AS size 
+FROM information_schema.TABLES   
+WHERE engine='myisam' AND table_schema NOT IN ('information_schema', 'performance_schema', 'mysql');
