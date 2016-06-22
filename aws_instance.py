13c60ca844790180596e6c0010f0f4f9764064a0
csterling <csterling@newmediadenver.com>
2016-06-22 17:03:18 -0600

    Needs the full kwarg?

--- aws_instance.py
+++ aws_instance.py
@@ -118,7 +118,7 @@ def move_volume(volume_id, old_instance_id, new_instance_id, device_name):
 
   # Setup the SSH commands
   ssh_cmd = "ssh -p22 -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no {user}@{host}"
-  umount_cmd = ssh_cmd + " sudo umount -f {device}"
+  umount_cmd = ssh_cmd + " sudo umount --force {device}"
   mmkdir_cmd = ssh_cmd + " sudo mkdir -p {folder}"
   mount_cmd = ssh_cmd + " sudo mount {device}"
   #boto3.client('ec2').describe_tags(Filters=[{"Name":"resource-id","Values":["i-318ca4c6"]}, {"Name":"key","Values":["Environment"]}])['Tags'][0]['Value']
