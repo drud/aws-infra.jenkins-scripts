cacc34a94d151cfc8b5d817db865de029b924e44
csterling <csterling@newmediadenver.com>
2016-08-02 13:28:17 -0600

    Fixing some paths - adding the same flags as ansible for a more fair comparison.

--- file-system-rsync.sh
+++ file-system-rsync.sh
@@ -6,8 +6,10 @@ if [ -z "$1" -o -z "$2" ]; then
 fi
 SRC_SERVER=$1
 DEST_SERVER=$2
-# Place the SSH key
-rsync /var/jenkins_home/.ssh/aws.pem ubuntu@$DEST_SERVER:/tmp/aws.pem 
+# Place the SSH key on the DEST server for consumption by the rsync script
+rsync --rsh 'ssh -i /var/jenkins_home/.ssh/aws.pem -o StrictHostKeyChecking=no' /var/jenkins_home/.ssh/aws.pem ubuntu@$DEST_SERVER:/tmp/aws.pem 
+
 # Set the HOSTNAME for the ssh-generator
 HOSTNAME=$DEST_SERVER
-RSYNC_SCRIPT=(`eval $(/var/jenkins_home/workspace/jenkins-scripts/ssh-generator.sh "rsync.sh $SRC_SERVER $DEST_SERVER" env)`)
+# Call the script and record the results
+`eval $(/var/jenkins_home/workspace/jenkins-scripts/ssh-generator.sh "rsync.sh $SRC_SERVER $DEST_SERVER" env)`
--- rsync.sh
+++ rsync.sh
@@ -4,9 +4,9 @@
 #SRC_SERVER
 #DEST_SERVER
 
-rsync -F --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path=\"sudo rsync\" ubuntu@$SRC_SERVER:/etc/nginx/sites-available /etc/nginx/
-rsync -F --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path=\"sudo rsync\" ubuntu@$SRC_SERVER:/etc/nginx/sites-enabled /etc/nginx/
-rsync -F --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path=\"sudo rsync\" ubuntu@$SRC_SERVER:/var/www/ /var/
+rsync -F --compress --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path=\"sudo rsync\" ubuntu@$SRC_SERVER:/etc/nginx/sites-available /etc/nginx/
+rsync -F --compress --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path=\"sudo rsync\" ubuntu@$SRC_SERVER:/etc/nginx/sites-enabled /etc/nginx/
+rsync -F --compress --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path=\"sudo rsync\" ubuntu@$SRC_SERVER:/var/www/ /var/
 
 # Remove the SSH key
 rm -rf /tmp/aws.pem
