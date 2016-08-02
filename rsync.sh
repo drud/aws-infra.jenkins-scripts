b3ecb91c94b465414133ccceb2c3d20963868fea
csterling <csterling@newmediadenver.com>
2016-08-02 14:52:59 -0600

    Fixing key perms after rsync copy.

--- rsync.sh
+++ rsync.sh
@@ -8,6 +8,7 @@
 # We are SSH-ed into a remote host and calling this script on that host
 # Communication on Port 22 is available
 # The standard ubuntu AWS deploy key has been placed at /tmp/aws.pem
+chmod 0400 /tmp/aws.pem
 
 # Call rsync on the different nodes.
 rsync -F --compress --archive --progress --stats --rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' --rsync-path="sudo rsync" ubuntu@$SRC_SERVER:/etc/nginx/sites-available /etc/nginx/
