795473ecad5c70f5219643a80e8f997305c76dd4
csterling <csterling@newmediadenver.com>
2016-08-03 08:21:15 -0600

    We don't need to include everything.

--- rsync.sh
+++ rsync.sh
@@ -40,7 +40,6 @@ for WEB_ROOT in $(find /var/www -maxdepth 2 -type l -name current -print); do
 	--stats \
 	--rsh 'ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no' \
 	--rsync-path="sudo mkdir -p /var/www/$BAGNAME/releases && sudo rsync" \
-	--include */
 	--exclude .git/ \
 	$CURRENT_RELEASE ubuntu@DEST_SERVER:/var/www/$BAGNAME/releases/
     # Copy (or recreate) the current symlink - remember, you are on the src host now
