bc9b7fa7177a813e66ffc9fe79c7b265715ed453
csterling <csterling@newmediadenver.com>
2016-08-03 08:34:35 -0600

    Adding sudo.

--- rsync.sh
+++ rsync.sh
@@ -43,7 +43,7 @@ for WEB_ROOT in $(find /var/www -maxdepth 2 -type l -name current -print); do
 	--exclude .git/ \
 	$CURRENT_RELEASE ubuntu@$DEST_SERVER:/var/www/$BAGNAME/releases/
     # Copy (or recreate) the current symlink - remember, you are on the src host now
-    ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no ubuntu@$DEST_SERVER "ln -s $CURRENT_RELEASE /var/www/$BAGNAME/current"
+    ssh -i /tmp/aws.pem -o StrictHostKeyChecking=no ubuntu@$DEST_SERVER "sudo ln -s $CURRENT_RELEASE /var/www/$BAGNAME/current"
 done
 
 # Remove the SSH key
