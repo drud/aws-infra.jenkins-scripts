03e27685a0085d665150e9e5708f90b9130c90e7
csterling <csterling@newmediadenver.com>
2016-08-05 15:54:47 -0600

    Per @frodopwns, removing the token and unsealing stuff.

--- new_client_databag.py
+++ new_client_databag.py
@@ -6,19 +6,12 @@ from collections import OrderedDict
 import hvac
 
 def get_vault_client():
-    vault_token = os.environ.get('GITHUB_TOKEN', "NO TOKEN FOUND") if 'GITHUB_TOKEN' in os.environ else os.environ.get('VAULT_TOKEN', "NO TOKEN FOUND")
+    vault_token = os.environ.get('GITHUB_TOKEN', "NO TOKEN FOUND")
     if vault_token == "NO TOKEN FOUND":
-        print "Could not find GITHUB_TOKEN or VAULT_TOKEN in your environment. Have you run `drud secret auth`?"
+        print "Could not find GITHUB_TOKEN in your environment. Have you run `drud secret auth`?"
         sys.exit(1)
-    vault_client = hvac.Client(url='https://sanctuary.drud.io:8200', token=vault_token, verify=False)
+    vault_client = hvac.Client(url='https://sanctuary.drud.io:8200')
     
-    if vault_client.is_initialized() and vault_client.is_sealed():
-        try:
-            vault_client.unseal(os.getenv('VAULT_KEY_1', ''))
-            vault_client.unseal(os.getenv('VAULT_KEY_2', ''))
-            vault_client.unseal(os.getenv('VAULT_KEY_3', ''))
-        except:
-            pass
     if vault_client.is_initialized() and not vault_client.is_sealed():
         if not vault_client.is_authenticated():
             vault_client.auth_github(vault_token)
