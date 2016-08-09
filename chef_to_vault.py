c184fb278d1d4d636633de0869741daf23537004
csterling <csterling@newmediadenver.com>
2016-08-09 10:31:34 -0600

    Don't verify.

--- chef_to_vault.py
+++ chef_to_vault.py
@@ -7,19 +7,24 @@ import hvac
 import databag
 import click
 from pprint import pprint as p
+import requests.packages.urllib3
 
 def get_vault_client():
     """
     Return a vault client if possible.
     """
+    # Disable warnings
+    #requests.packages.urllib3.disable_warnings()
     vault_addr = os.getenv("VAULT_ADDR", "https://sanctuary.drud.io:8200")
     vault_token = os.getenv('GITHUB_TOKEN', False)
     if not vault_addr or not vault_token:
         print "You must provide both VAULT_ADDR and GITHUB_TOKEN environment variables."
         print "(Have you authenticated with `drud secret auth` to create your GITHUB_TOKEN?)"
         sys.exit(1)
+    else:
+        print vault_addr, vault_token
 
-    vault_client = hvac.Client(url=vault_addr)
+    vault_client = hvac.Client(url=vault_addr, verify=False)
     vault_client.auth_github(vault_token)
 
     if vault_client.is_initialized() and vault_client.is_sealed():
@@ -104,8 +109,6 @@ def sync(dest, debug):
                     vault_client.write(dest, value=databag)
                 else:
                     print "We don't know what to do with this bag"
-            
-
 
 
 if __name__ == '__main__':
