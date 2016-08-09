76af7698c4e43965e495dbb3d421a5ad08fd6340
csterling <csterling@newmediadenver.com>
2016-08-09 13:34:11 -0600

    Adding the better function.

--- databag-proxy-servers.py
+++ databag-proxy-servers.py
@@ -11,6 +11,32 @@ proxy_container="nmdproxy"
 #   "server web02.newmediadenver.com:80;",
 #   "server web04.newmediadenver.com:80;"
 # ]
+def get_vault_client():
+    """
+    Return a vault client if possible.
+    """
+    # Disable warnings for the insecure calls
+    requests.packages.urllib3.disable_warnings()
+    vault_addr = os.getenv("VAULT_ADDR", "https://sanctuary.drud.io:8200")
+    vault_token = os.getenv('GITHUB_TOKEN', False)
+    if not vault_addr or not vault_token:
+        print "You must provide both VAULT_ADDR and GITHUB_TOKEN environment variables."
+        print "(Have you authenticated with `drud secret auth` to create your GITHUB_TOKEN?)"
+        sys.exit(1)
+
+    vault_client = hvac.Client(url=vault_addr, verify=False)
+    vault_client.auth_github(vault_token)
+
+    if vault_client.is_initialized() and vault_client.is_sealed():
+        print "Vault is initialized but sealed."
+        sys.exit(1)
+
+    if not vault_client.is_authenticated():
+        print "Could not get auth."
+        sys.exit(1)
+
+    return vault_client
+
 def remove_server(servers, server_to_remove, cluster):
   """
   Removes a server from the list.
