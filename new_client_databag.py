18771a12af5a46f54d95bfc1734caa0df818c9f5
csterling <csterling@newmediadenver.com>
2016-08-09 12:01:47 -0600

    Adding known good get client function from chef_to_vault.py

--- new_client_databag.py
+++ new_client_databag.py
@@ -5,19 +5,33 @@ import click
 from collections import OrderedDict
 import hvac
 import os
+import requests.packages.urllib3
 
 def get_vault_client():
-    vault_token = os.environ.get('GITHUB_TOKEN', "NO TOKEN FOUND")
-    if vault_token == "NO TOKEN FOUND":
-        print "Could not find GITHUB_TOKEN in your environment. Have you run `drud secret auth`?"
+    """
+    Return a vault client if possible.
+    """
+    # Disable warnings for the insecure calls
+    requests.packages.urllib3.disable_warnings()
+    vault_addr = os.getenv("VAULT_ADDR", "https://sanctuary.drud.io:8200")
+    vault_token = os.getenv('GITHUB_TOKEN', False)
+    if not vault_addr or not vault_token:
+        print "You must provide both VAULT_ADDR and GITHUB_TOKEN environment variables."
+        print "(Have you authenticated with `drud secret auth` to create your GITHUB_TOKEN?)"
         sys.exit(1)
-    vault_client = hvac.Client(url='https://sanctuary.drud.io:8200')
-    
-    if vault_client.is_initialized() and not vault_client.is_sealed():
-        if not vault_client.is_authenticated():
-            vault_client.auth_github(vault_token)
-        if vault_client.is_authenticated():
-            return vault_client
+
+    vault_client = hvac.Client(url=vault_addr, verify=False)
+    vault_client.auth_github(vault_token)
+
+    if vault_client.is_initialized() and vault_client.is_sealed():
+        print "Vault is initialized but sealed."
+        sys.exit(1)
+
+    if not vault_client.is_authenticated():
+        print "Could not get auth."
+        sys.exit(1)
+
+    return vault_client
 
 def to_boolean(x):
     return x.lower() == 'true'
