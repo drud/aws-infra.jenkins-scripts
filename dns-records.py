bfa637fe40e340a1127d241a0e7e8939eb46c2a5
TopherGopher <topher@develops.guru>
2017-03-06 13:50:10 -0700

    Adding a delete_record operation.

--- dns-records.py
+++ dns-records.py
@@ -3,7 +3,11 @@ import click
 import boto
 from boto.route53.record import ResourceRecordSets
 
-@click.command()
+@click.group()
+def context():
+    pass
+
+@context.command()
 @click.option('--name', 
     help='The name of the dns entry to create.'
 )
@@ -15,9 +19,8 @@ from boto.route53.record import ResourceRecordSets
     type=click.Choice(['A', 'CNAME'])
 )
 @click.option('--zone', 
-    help='The Hosted Zone ID to create the entry in.'
+    help='The Hosted Zone ID to create the entry in'
 )
-
 def create_record(name, value, type, zone):
     """Create DNS entries in route53"""
     conn = boto.connect_route53()
@@ -26,5 +29,27 @@ def create_record(name, value, type, zone):
     change.add_value(value)
     result = change_set.commit()
 
+@context.command()
+@click.option('--name', 
+    help='The name of the dns entry to delete.'
+)
+@click.option('--type',
+    help='The type of DNS Record to delete',
+    type=click.Choice(['A', 'CNAME'])
+)
+@click.option('--zone', 
+    help='The Hosted Zone ID to delete the entry from'
+)
+def delete_record(name, type, zone):
+    """Delete DNS entries in route53"""
+    conn = boto.connect_route53()
+    zones = {z.id : z for z in conn.get_zones()}
+    if type == "CNAME":
+        status = zones[zone].delete_cname(name)
+    elif type == "A":
+        status = zones[zone].delete_a(name)
+    else:
+        exit("Unsupported record type")
+
 if __name__ == '__main__':
-    create_record()
+    context()
