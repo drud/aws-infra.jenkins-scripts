19f14ef53fe85babd7101c379a16cc0b76fed758
Nick McCrory <n.mccrory@icloud.com>
2017-08-12 22:22:55 -0700

    Fixed file naming issue with artifact download.

--- ghlabel-sync.sh
+++ ghlabel-sync.sh
@@ -1,10 +1,9 @@
 # Download binary from CircleCI build
-curl -i https://circleci.com/api/v1.1/project/github/nmccrory/ghlabel/42/artifacts$CIRCLE_TOKEN | grep -o 'https://[^"]*' > artifacts.txt
-<artifacts.txt xargs -P4 -I % wget %$CIRCLE_TOKEN
+curl -o ghlabel https://42-99512752-gh.circle-artifacts.com/0/home/circleci/go/src/github.com/drud/ghlabel/bin/linux/ghlabel$CIRCLE_TOKEN
 
 # TEMP: Print out a log of the filesystem
 ls -l
 
 # Set permission to execute binary
 # Step 2: Execute binary using Drud as our organization and community as our reference repository.
-chmod +x ./ghlabel$CIRCLE_TOKEN && ./ghlabel$CIRCLE_TOKEN --org=drud --ref=community -r
+chmod +x ./ghlabel && ./ghlabel --org=drud --ref=community -r
