c95d426f788269bc5806556de01d808f4d9120ef
Nick McCrory <n.mccrory@icloud.com>
2017-08-13 05:32:07 -0700

    Added a cleanup step to the script.

--- ghlabel-sync.sh
+++ ghlabel-sync.sh
@@ -1,9 +1,10 @@
 # Download binary from CircleCI build
-curl -o ghlabel https://42-99512752-gh.circle-artifacts.com/0/home/circleci/go/src/github.com/drud/ghlabel/bin/linux/ghlabel$CIRCLE_TOKEN
+curl -o /bin/ghlabel https://42-99512752-gh.circle-artifacts.com/0/home/circleci/go/src/github.com/drud/ghlabel/bin/linux/ghlabel$CIRCLE_TOKEN
 
-# TEMP: Print out a log of the filesystem
-ls -l
+# Set permission to execute binary.
+# Execute binary using Drud as our organization and community as our reference repository.
+# (This is the actual sync)
+chmod +x ./bin/ghlabel && ./bin/ghlabel --org=drud --ref=community -r
 
-# Set permission to execute binary
-# Step 2: Execute binary using Drud as our organization and community as our reference repository.
-chmod +x ./ghlabel && ./ghlabel --org=drud --ref=community -r
+# Clean up
+rm ./bin/ghlabel -f
