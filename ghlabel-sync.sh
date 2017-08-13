5d3dad6dc0f7186b862688b7ecaa1d21f38448c3
Nick McCrory <n.mccrory@icloud.com>
2017-08-13 05:48:42 -0700

    Went back old artifact file path

--- ghlabel-sync.sh
+++ ghlabel-sync.sh
@@ -1,10 +1,10 @@
 # Download binary from CircleCI build
-curl -o /bin/ghlabel https://42-99512752-gh.circle-artifacts.com/0/home/circleci/go/src/github.com/drud/ghlabel/bin/linux/ghlabel$CIRCLE_TOKEN
+curl -o ./ghlabel https://42-99512752-gh.circle-artifacts.com/0/home/circleci/go/src/github.com/drud/ghlabel/bin/linux/ghlabel$CIRCLE_TOKEN
 
 # Set permission to execute binary.
 # Execute binary using Drud as our organization and community as our reference repository.
 # (This is the actual sync)
-chmod +x ./bin/ghlabel && ./bin/ghlabel --org=drud --ref=community -r
+chmod +x ./ghlabel && ./ghlabel --org=drud --ref=community -r
 
 # Clean up
-rm ./bin/ghlabel -f
+rm -f ./ghlabel
